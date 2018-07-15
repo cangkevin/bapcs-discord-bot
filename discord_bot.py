8d7da1f892250b2f0ac6201d32c222bce1ac830e
cangkevin <cangkevin@gmail.com>
2018-07-15 01:13:17 -0500

    add color-coding of submissions

--- discord_bot.py
+++ discord_bot.py
@@ -10,6 +10,41 @@ from decouple import config
 logging.basicConfig(level=logging.INFO)
 
 client = discord.Client()
+colors = {
+    'BLUE': discord.Color(0x0066ff),
+    'RED': discord.Color(0xcc0000),
+    'ORANGE': discord.Color(0xffcc66),
+    'GREEN': discord.Color(0x00ff33),
+    'WHITE': discord.Color(0xffffff),
+    'MAGENTA': discord.Color(0x9966ff),
+    'YELLOW': discord.Color(0xffff66)
+}
+
+def embed_color(flair):
+    if flair is None:
+        return colors['MAGENTA']
+
+    flair_text = flair.lower()
+    return {
+        'case': colors['BLUE'],
+        'cooler': colors['BLUE'],
+        'fan': colors['ORANGE'],
+        'hdd': colors['BLUE'],
+        'keyboard': colors['GREEN'],
+        'mouse': colors['GREEN'],
+        'ram': colors['BLUE'],
+        'mobo': colors['BLUE'],
+        'prebuilt': colors['WHITE'],
+        'controller': colors['GREEN'],
+        'cpu': colors['BLUE'],
+        'gpu': colors['BLUE'],
+        'headphones': colors['GREEN'],
+        'monitor': colors['RED'],
+        'psu': colors['BLUE'],
+        'ssd': colors['BLUE'],
+        'meta': colors['YELLOW'],
+    }.get(flair_text, colors['MAGENTA'])
+
 
 async def check_queue(q):
     await client.wait_until_ready()
@@ -20,7 +55,8 @@ async def check_queue(q):
                 submission = q.get_nowait()
                 em = discord.Embed(title=submission['title'],
                                     url=submission['url'],
-                                    description=submission['reddit_url'])
+                                    description=submission['reddit_url'],
+                                    colour=embed_color(submission['type']))
                 await client.send_message(channel, embed=em)
             except Exception as e: # if queue is empty
                 pass
